1685c59848fd734aee9bffe5e026a3f9e5c8aff8
Afonso Barrenha <afonsobarrenha@gmail.com>
2018-02-04 03:00:34 -0200

    Início do uso do SpringBoot e inclusão dos GitIgnore.

--- implementation/SpringBoot/src/main/java/com/pocs/SpringBoot/Application.java
+++ implementation/SpringBoot/src/main/java/com/pocs/SpringBoot/Application.java
@@ -2,9 +2,19 @@ package com.pocs.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String ola() {
+		return "Ola, Bem vindo ao sistema Lista VIPs";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
