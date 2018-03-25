971e6305d05dfe958a4b018a439a922db6152484
afonsobarrenha <afonsobarrenha@gmail.com>
2018-03-25 16:44:07 -0300

    Corrigindo os pacotes dos testes, e consolidando os testes por recursos Spring necessários.

--- implementation/SpringBoot/src/test/java/com/example/demo/controller/MockMvcTest.java
+++ implementation/SpringBoot/src/test/java/com/example/demo/controller/MockMvcTest.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,14 +20,28 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class InfoControllerTest {
+public class MockMvcTest {
 
 	@Autowired
-	private MockMvc mvc;
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ConvidadoController controler;
 
 	@Test
-	public void getI() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(content().string(containsString("springboot")));
+	public void contextLoads() {
+		assertThat(controler).isNotNull();
+	}
+
+	@Test
+	public void getIndex() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().string(containsString("springboot")));
+	}
+
+	@Test
+	public void getListar() throws Exception {
+		this.mockMvc.perform(get("/listar")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("afonsobarrenha@gmail.com")));
 	}
 }
