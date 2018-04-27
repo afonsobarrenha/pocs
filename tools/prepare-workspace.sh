bcdd644988301a6597fb908deabf31ad7b2e7727
afonsobarrenha <afonsobarrenha@gmail.com>
2018-04-26 23:47:36 -0300

    Google Cloud SDK!

--- tools/prepare-workspace.sh
+++ tools/prepare-workspace.sh
@@ -38,6 +38,19 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minik
 echo '>>> Instalando o Kubernetes'
 snap install kubectl --classic
 
+echo '>>> Instalando o Google Cloud SDK'
+# Create an environment variable for the correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
 echo '>> Clonando o projeto POCS'
 echo '>>> Mudando diretório para pasta HOME'
 cd ~
