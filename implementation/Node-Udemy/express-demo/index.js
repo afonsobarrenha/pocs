a5f35470beb4681172cf1e7ec61e77e7dd787d44
Afonso Henrique Barrenha <afonsobarrenha@gmail.com>
2019-01-03 14:42:57 -0200

    GET.

--- implementation/Node-Udemy/express-demo/index.js
+++ implementation/Node-Udemy/express-demo/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 var app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
     res.send('helloworld');
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3,4,5]);
+    res.send(courses);
 })
 
-app.get('/api/courses/:year/:month', (req, res) => {
-    res.send(`Param year:${req.params.year}, Param month: ${req.params.month}. Query: ${req.query}`);
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course not found.');
+    res.send(course);
 })
 
 const port = process.env.PORT || 3000
