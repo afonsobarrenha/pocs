dcff19f08a2a681c2237b64f3d72ce9263673b4e
Afonso Henrique Barrenha <afonsobarrenha@gmail.com>
2019-01-03 16:11:06 -0200

    POST.

--- implementation/Node-Udemy/express-demo/index.js
+++ implementation/Node-Udemy/express-demo/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var app = express();
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -18,6 +19,16 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('Course not found.');
+    res.send(course);
+})
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    }
+
+    courses.push(course);
     res.send(course);
 })
 
